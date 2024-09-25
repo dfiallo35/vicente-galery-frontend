62c945cfae0ec2d459ada5c8847e4dc347a04a39
Dennis Fiallo <dfiallo35@gmail.com>
2024-09-25 12:50:36 -0400

    initialize project

--- galery/src/app/app.component.ts
+++ galery/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GaleryCardListComponent } from "./components/galery-card-list/galery-card-list.component";
-import { GaleryCardComponent } from "./components/galery-card/galery-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GaleryCardListComponent, GaleryCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
